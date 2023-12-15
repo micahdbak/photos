e4f2675c67e47cd5065ce34b8ed89e47a5d1dfd6
Micah Baker <micahb923@gmail.com>
2023-12-14 17:31:49 -0800

    Added blank page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
 import React from 'react';
 
+import {
+  Button,
+  Dropdown,
+  Flex,
+  Icon,
+  Navigation,
+  helpers
+} from 'mdb-react-components';
+
 function App() {
   return (
-    <p>Hello, world!</p>
+    <Flex.Container
+      flow='row nowrap'
+      alignItems='stretch'
+      style={{
+        width: '100vw',
+        height: '100vh'
+      }}
+    >
+      <Navigation.Sidebar>
+        <h1 style={{ margin: '8px', fontSize: '14pt' }}>
+          Analog Gallery
+        </h1>
+        <Button
+          className='primary small'
+          style={{ marginTop: 'auto' }}
+          href='https://github.com/micahdbak/photos'
+        >
+          <Icon.Link />
+          GitHub
+        </Button>
+      </Navigation.Sidebar>
+      <Flex.Item flex='1 0' style={{ position: 'relative' }}>
+        <p
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)'
+          }}
+        >
+          Under construction; export more soon!
+        </p>
+      </Flex.Item>
+    </Flex.Container>
   );
 }
 
