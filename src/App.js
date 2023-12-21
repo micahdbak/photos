1fb8a9a6dcfd298de452fb5705da1922d86e4118
Micah Baker <micahb923@gmail.com>
2023-12-20 23:40:22 -0800

    Updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import {
   Button,
   Dropdown,
   Flex,
-  Icon,
-  Navigation,
-  helpers
+  Icon
 } from 'mdb-react-components';
 
 function App() {
+  const [years, setYears] = useState([]);
+
+  const sidebarItemStyle = {
+    justifyContent: 'flex-start'
+  };
+
+  // on page load
+  useEffect(() => {
+    async function loadPhotos() {
+      const photos = await axios.get(process.env.PUBLIC_URL + '/photos.json');
+      setYears(photos.data);
+    }
+
+    loadPhotos();
+  }, []);
+
   return (
     <Flex.Container
       flow='row nowrap'
@@ -19,19 +34,68 @@ function App() {
         height: '100vh'
       }}
     >
-      <Navigation.Sidebar>
-        <h1 style={{ margin: '8px', fontSize: '14pt' }}>
-          Analog Gallery
-        </h1>
-        <Button
-          className='primary small'
-          style={{ marginTop: 'auto' }}
-          href='https://github.com/micahdbak/photos'
-        >
-          <Icon.Link />
-          GitHub
+      <Flex.Container
+        flow='column nowrap'
+        gap='4px'
+        style={{
+          borderRight: '1px solid var(--mdb-border-color)',
+          padding: '8px',
+          backgroundColor: 'var(--mdb-background-color-alt)'
+        }}
+      >
+        <Button className='primary icon' href='/'>
+          <Icon.Option style={{ transform: 'scale(-1, -1)' }} />
         </Button>
-      </Navigation.Sidebar>
+        <Flex.Item>
+          <hr
+            style={{
+              marginTop: '4px',
+              marginBottom: '4px',
+              border: 'none',
+              borderTop: '1px solid var(--mdb-border-color)'
+            }}
+          />
+        </Flex.Item>
+        <Button className='icon' href='/photos'>
+          <Icon.Film />
+        </Button>
+        <Button className='icon' href='https://github.com/micahdbak'>
+          <Icon.Link />
+        </Button>
+      </Flex.Container>
+      <Flex.Container
+        flow='column nowrap'
+        gap='4px'
+        style={{
+          borderRight: '1px solid var(--mdb-border-color-alt)',
+          padding: '8px'
+        }}
+      >
+        <h1 style={{ margin: '8px', fontSize: '14pt' }}>
+          Photo Gallery
+        </h1>
+        {years.map(year =>
+          <Dropdown
+            className='transparent small'
+            style={sidebarItemStyle}
+            icon='arrow'
+            iconAlign='left'
+            text={year.dir}
+            isStaticDropdown={true}
+            isDroppedDownInitially={true}
+          >
+            {year.dirs.map(collection =>
+              <Button
+                className='transparent small'
+                style={sidebarItemStyle}
+                depth={1}
+              >
+                {collection.title}
+              </Button>
+            )}
+          </Dropdown>
+        )}
+      </Flex.Container>
       <Flex.Item flex='1 0' style={{ position: 'relative' }}>
         <p
           style={{
@@ -41,7 +105,7 @@ function App() {
             transform: 'translate(-50%, -50%)'
           }}
         >
-          Under construction; export more soon!
+          Under construction; expect more soon!
         </p>
       </Flex.Item>
     </Flex.Container>
